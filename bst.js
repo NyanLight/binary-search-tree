238c39c7dd319e994ab5075db7af60196a8054ce
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-03-26 22:41:38 +0400

    create buildTree function

--- bst.js
+++ bst.js
@@ -1,5 +1,16 @@
 export class Tree {
-    constructor(array) {
-        this.array = array; 
-    }
+  constructor(array) {
+    this.array = array;
+  }
+
+  root = buildTree();
+
+  buildTree(start = 0, end = array.length - 1) {
+    if (start > end) return null;
+    const mid = Math.floor(start + end / 2);
+    const node = new Node (this.array[mid]);
+    node.left = buildTree(start, mid - 1);
+    node.right = this.buildTree(mid + 1, end);
+    return node;
+  }
 }
